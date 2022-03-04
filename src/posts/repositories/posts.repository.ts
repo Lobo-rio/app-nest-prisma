907651c595a8bc65f8813ed37997f5f14c9480ce
Gilberto <gsilvamedeiros@gmail.com>
2022-03-04 14:34:07 -0300

    update initial

---
+++ src/posts/repositories/posts.repository.ts
@@ -0,0 +1,107 @@
+import { Injectable, NotAcceptableException } from '@nestjs/common';
+import { Prisma } from '@prisma/client';
+import { PrismaService } from '../../prisma/prisma.service';
+import { CreatePostDto } from '../dto/create-post.dto';
+import { UpdatePostDto } from '../dto/update-post.dto';
+import { PostEntity } from '../entities/post.entity';
+
+@Injectable()
+export class PostsRepository {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async create(createPostDto: CreatePostDto): Promise<PostEntity> {
+    const { authorEmail } = createPostDto;
+
+    delete createPostDto.authorEmail;
+
+    const user = await this.prisma.user.findUnique({
+      where: {
+        email: authorEmail,
+      },
+    });
+
+    if (!user) throw new NotAcceptableException('Author not found!');
+
+    const data: Prisma.PostCreateInput = {
+      ...createPostDto,
+      author: {
+        connect: {
+          email: authorEmail,
+        },
+      },
+    };
+
+    return this.prisma.post.create({ data });
+  }
+
+  async findAll(): Promise<PostEntity[]> {
+    return this.prisma.post.findMany({
+      include: {
+        author: true,
+      },
+    });
+  }
+
+  async findOne(id: number): Promise<PostEntity> {
+    return this.prisma.post.findUnique({
+      where: { id },
+      include: {
+        author: {
+          select: {
+            name: true,
+            email: true,
+          },
+        },
+      },
+    });
+  }
+
+  async update(id: number, updatePostDto: UpdatePostDto): Promise<PostEntity> {
+    const { authorEmail } = updatePostDto;
+
+    if (!authorEmail) {
+      return this.prisma.post.update({
+        where: { id },
+        data: updatePostDto,
+      });
+    }
+
+    delete updatePostDto.authorEmail;
+
+    const user = await this.prisma.user.findUnique({
+      where: {
+        email: authorEmail,
+      },
+    });
+
+    if (!user) throw new NotAcceptableException('Author not found!');
+
+    const data: Prisma.PostUpdateInput = {
+      ...updatePostDto,
+      author: {
+        connect: {
+          email: authorEmail,
+        },
+      },
+    };
+
+    return this.prisma.post.update({
+      where: { id },
+      data,
+      include: {
+        author: {
+          select: {
+            name: true,
+            updatedAt: true,
+          },
+        },
+      },
+    });
+  }
+
+  async remove(id: number): Promise<PostEntity> {
+    return this.prisma.post.delete({
+      where: { id },
+    });
+  }
+}
